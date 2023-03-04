d36ea952d2c71c51447724e8a57046e9d2d05c03
lgaricoix <lucasgaricoix@gmail.com>
2023-03-04 12:29:57 -0300

    fix: run.sh path

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar build/libs/myclinicpay-0.0.1-SNAPSHOT.jar -Dserver.port=8082 -Duser.timezone=America/Sao_Paulo
+java -jar /home/ec2-user/server/libs/myclinicpay-0.0.1-SNAPSHOT.jar -Dserver.port=8082 -Duser.timezone=America/Sao_Paulo
