bb012c0c5bd949aa27c340f5392f8bb3d9071e12
lgaricoix <lucasgaricoix@gmail.com>
2023-03-12 01:32:14 -0300

    feat: year on income data

--- src/main/kotlin/br/com/myclinicpay/domain/model/payment/PaymentOverMonth.kt
+++ src/main/kotlin/br/com/myclinicpay/domain/model/payment/PaymentOverMonth.kt
@@ -1,8 +1,7 @@
 package br.com.myclinicpay.domain.model.payment
 
-import java.time.Month
-
 class PaymentOverMonth(
+    val year: Int,
     val month: String,
     val income: Double,
     val expense: Double,
