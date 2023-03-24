d7c1d43b36a77fe4134f69d5196d3f3a20c066dd
lgaricoix <lucasgaricoix@gmail.com>
2023-03-24 11:16:41 -0300

    feat: update run.sh jar path

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /artifacts/build/libs/myclinicpay-0.0.1-SNAPSHOT.jar -Dserver.port=8082 -Duser.timezone=America/Sao_Paulo
+java -jar /root/build/libs/myclinicpay-0.0.1-SNAPSHOT.jar -Dserver.port=8082 -Duser.timezone=America/Sao_Paulo
